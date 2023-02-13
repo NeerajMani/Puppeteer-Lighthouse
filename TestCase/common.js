e64bc500806d46fc90dd8fd6ccee708c841abbf9
Neeraj Tirpathi <neerajmanitripathi@gmail.com>
2023-02-13 10:22:33 +0530

    Update common.js

--- TestCase/common.js
+++ TestCase/common.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 const launchBrowser = async (url) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer({
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    headless: true,
+  });
   const page = await browser.newPage();
   await page.goto(url);
   return {browser, page};
